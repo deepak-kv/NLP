82a530ea7984d20d93f3d6ddf47a61ca1f05cd3b
deepak-kv <sh2deepakkumar@gmail.com>
2025-03-07 19:10:04 +0530

    updated for unk token

--- utils/helper.py
+++ utils/helper.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from collections import defaultdict
 
 class CustomTokenizer:
@@ -15,8 +16,19 @@ class CustomTokenizer:
 
 class Vocabulary:
     def __init__(self):
-        self.word2idx = defaultdict(lambda: len(self.word2idx))  # Automatically assign new IDs
-        self.idx2word = {}
+        # Reserve index 0 for <UNK>
+        self.word2idx = {'<UNK>': 0}  # Initialize with <UNK> token
+        self.idx2word = {0: '<UNK>'}  # Reverse mapping
+        self.next_idx = 1  # Start assigning IDs from 1
+
+    def add_word(self, word):
+        """
+        Adds a word to the vocabulary if it doesn't already exist.
+        """
+        if word not in self.word2idx:
+            self.word2idx[word] = self.next_idx
+            self.idx2word[self.next_idx] = word
+            self.next_idx += 1
 
     def build_vocab(self, sentences):
         """
@@ -25,27 +37,29 @@ class Vocabulary:
         for sentence in sentences:
             tokens = tokenizer.tokenize(sentence)
             for token in tokens:
-                _ = self.word2idx[token]  # Assign an ID to each token
-
-        # Create a reverse mapping for decoding
-        self.idx2word = {idx: word for word, idx in self.word2idx.items()}
+                self.add_word(token)
 
     def get_token_ids(self, sentences, seq_length):
         """
         Converts a list of sentences into token IDs using the vocabulary.
         Pads or truncates each sentence to ensure a fixed sequence length.
+        Uses 0 (<UNK>) for out-of-vocabulary tokens.
         """
         token_ids = []
         for sentence in sentences:
             tokens = tokenizer.tokenize(sentence)
-            ids = [self.word2idx[token] for token in tokens]
+            ids = [self.word2idx.get(token, 0) for token in tokens]  # Use 0 for unknown tokens
             # Pad or truncate the sequence to the specified seq_length
             if len(ids) < seq_length:
-                ids += [0] * (seq_length - len(ids))  # Pad with 0 (assuming 0 is the padding token)
+                ids += [0] * (seq_length - len(ids))  # Pad with 0 (<UNK>)
             else:
                 ids = ids[:seq_length]  # Truncate to seq_length
             token_ids.append(ids)
         return np.array(token_ids).T  # Transpose to get shape (seq_length, num_sentences)
+
+
+
+
 
 # Input sentences
 #sentences = [
